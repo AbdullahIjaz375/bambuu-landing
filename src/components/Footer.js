84829335727108a1c65fe0dc049c34bdb9ddb6b2
AbdullahIjaz375 <abdullahijaz375@gmail.com>
2025-03-17 05:50:59 +0000

    Revert "add Youtube icon at landing page"
    This reverts commit 450cdf80d0a4934b05badd17e0b3d71041ed095b.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="border-t-8 flex flex-col space-y-20 border-[#B9F9C2] rounded-t-[20vh] mt-40 pt-24 pb-12 footer-background">
+    <div className="border-t-8 flex flex-col space-y-20  border-[#B9F9C2] rounded-t-[20vh] mt-40 pt-24 pb-12 footer-background">
       <div className="flex items-center justify-center">
         <img
           alt="bambuu"
@@ -21,35 +21,21 @@ const Footer = () => {
         </div>
         <div className="flex flex-row items-center justify-center space-x-12">
           <a href="https://www.instagram.com/bammbuu_languages/">
+            {" "}
             <img
-              alt="instagram"
+              alt="bambuu"
               src="/images/insta.png"
               className="w-auto h-auto"
             />
           </a>
           <a href="https://www.facebook.com/profile.php?id=61565466403338">
-            <img
-              alt="facebook"
-              src="/images/fb.png"
-              className="w-auto h-auto"
-            />
+            {" "}
+            <img alt="bambuu" src="/images/fb.png" className="w-auto h-auto" />
           </a>
-          
-          <a href="https://www.youtube.com/channel/YourChannelID">
-            <img
-
-              alt="youtube"
-              src="/images/youtube24.jpg"
-              className="w-auto h-auto"
-              style={{
-                transform:'scale(1.2)',
-                opacity:'0.8',
-              }}
-             
-            />
-          </a>
+          {/* <img alt="bambuu" src="/images/x.png" className="w-auto h-auto" /> */}
         </div>
         <div className="flex flex-row items-center space-x-6">
+          {" "}
           <Link to="/privacy-policy">
             <h1 className="text-xl text-[#6D6D6D]">Privacy Policy</h1>
           </Link>
